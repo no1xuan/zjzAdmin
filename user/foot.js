f356be7538297fc8f3f3c6b3b32ec570185fa77f
贫困的蚊子 <24677102@qq.com>
2024-10-07 01:34:30 +0800

    更新管理员后台

---
+++ user/foot.js
@@ -0,0 +1,6 @@
+document.writeln("        </div>");
+document.writeln("        <div class=\'layadmin-body-shade\' layadmin-event=\'shade\'></div>");
+document.writeln("    </div>");
+document.writeln("</div>");
+document.writeln("</body>");
+document.writeln("</html>");
